6485ef29d318b6ac6911cd1fccf630a3a899bd39
Bob Jackson <wangyousong@9191offer.com>
2025-02-09 16:07:21 +0800

    build(deps): 添加 @ant-design/v5-patch-for-react-19 依赖
    - 在 App.tsx 中导入 @ant-design/v5-patch-for-react-19- 在 package.json 中添加 @ant-design/v5-patch-for-react-19 依赖

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import BookList from "./ BookList.tsx";
+import '@ant-design/v5-patch-for-react-19';
 
 function App() {
     return (
