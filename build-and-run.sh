225478ba551080b90c5dc085b086a2f93941e9c7
Bob Jackson <wangyousong@9191offer.com>
2025-02-15 12:40:36 +0800

    build:调整 Docker容器端口映射
    - 将 Docker 容器的端口映射从 80改为 3000- 保持主机端口 10086 不变

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,4 +2,4 @@ git pull
 docker stop growth-frontend-ui
 docker rm growth-frontend-ui
 docker build -t growth-frontend-ui .
-docker run -d -p 10086:80 --restart always --name growth-frontend-ui --network www_wordpress-network growth-frontend-ui
+docker run -d -p 10086:3000 --restart always --name growth-frontend-ui --network www_wordpress-network growth-frontend-ui
