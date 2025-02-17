f9f17baa3a2cbc70f33e4115d59b268283063928
Bob Jackson <wangyousong@9191offer.com>
2025-02-17 22:54:38 +0800

    feat(BookList): 为书籍列表添加搜索功能
    - 在 BookList 组件中添加了搜索输入框和相关逻辑
    - 实现了根据标题搜索书籍的功能
    - 添加了搜索图标和高亮显示搜索结果
    - 更新了表格列定义，集成了搜索功能
    -引入了 react-highlight-words库用于高亮搜索结果

--- src/BookList.tsx
+++ src/BookList.tsx
@@ -1,8 +1,23 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import {Button, Flex, Image, message, Popconfirm, Switch, Table, TablePaginationConfig} from 'antd';
-import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {
+    Button,
+    Flex,
+    Image,
+    Input,
+    InputRef,
+    message,
+    Popconfirm,
+    Space,
+    Switch,
+    Table,
+    TableColumnType,
+    TablePaginationConfig
+} from 'antd';
+import {DeleteOutlined, EditOutlined, SearchOutlined} from "@ant-design/icons";
 import BookForm from './BookForm';
 import {format} from 'date-fns';
+import {FilterDropdownProps} from "antd/es/table/interface";
+import Highlighter from 'react-highlight-words';
 
 interface Book {
     id: string;
@@ -24,11 +39,110 @@ interface Pagination {
     total: number;
 }
 
+type DataIndex = keyof Book;
+
 const BookList: React.FC = () => {
     const [books, setBooks] = useState<Book[]>([]);
     const [pagination, setPagination] = useState<Pagination>({current: 1, pageSize: 10, total: 0});
     const [editingBook, setEditingBook] = useState<Book | null>(null);
     const [isAddingBook, setIsAddingBook] = useState(false);
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef<InputRef>(null);
+
+    const handleSearch = (
+        selectedKeys: string[],
+        confirm: FilterDropdownProps['confirm'],
+        dataIndex: DataIndex,
+    ) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters: () => void) => {
+        clearFilters();
+        setSearchText('');
+    };
+
+    const getColumnSearchProps = (dataIndex: DataIndex): TableColumnType<Book> => ({
+        filterDropdown: ({setSelectedKeys, selectedKeys, confirm, clearFilters, close}) => (
+            <div style={{padding: 8}} onKeyDown={(e) => e.stopPropagation()}>
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                    style={{marginBottom: 8, display: 'block'}}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+                        icon={<SearchOutlined/>}
+                        size="small"
+                        style={{width: 90}}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{width: 90}}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({closeDropdown: false});
+                            setSearchText((selectedKeys as string[])[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered: boolean) => (
+            <SearchOutlined style={{color: filtered ? '#1677ff' : undefined}}/>
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex]
+                .toString()
+                .toLowerCase()
+                .includes((value as string).toLowerCase()),
+        filterDropdownProps: {
+            onOpenChange(open) {
+                if (open) {
+                    setTimeout(() => searchInput.current?.select(), 100);
+                }
+            },
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{backgroundColor: '#ffc069', padding: 0}}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
 
     const fetchBooks = useCallback(async (page: number, size: number) => {
         try {
@@ -155,7 +269,12 @@ const BookList: React.FC = () => {
     };
 
     const columns = [
-        {title: 'Title', dataIndex: 'title', key: 'title'},
+        {
+            title: 'Title',
+            dataIndex: 'title',
+            key: 'title',
+            ...getColumnSearchProps('title'),
+        },
         {title: 'Sub Title', dataIndex: 'subTitle', key: 'subTitle'},
         {
             title: 'Cover',
